959b5320ee0370f207190f337ef3fab08b47495d
Marcos Gabriel <mrkgab.069@gmail.com>
2026-04-26 23:22:03 -0300

    adição da aba cadastros

--- src/app/features/users/components/MenuComponent/index.js
+++ src/app/features/users/components/MenuComponent/index.js
@@ -9,62 +9,81 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import { AppRegistration, GroupAdd, Help, HomeOutlined, NoteAlt, QueryStats } from '@mui/icons-material';
+import { AppRegistration, ChevronRight, GroupAdd, Help, HomeOutlined, NoteAlt, QueryStats, } from '@mui/icons-material';
 import { HelpIcon } from '@mui/icons-material/Help';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function MenuComponent() {
+
+  //const navigate = useNavigate()
 
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation">
       <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                 <HomeOutlined />
-              </ListItemIcon>
-              <ListItemText primary='Inicio' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                 <GroupAdd />
-              </ListItemIcon>
-              <ListItemText primary='Cadastros' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                 <AppRegistration />
-              </ListItemIcon>
-              <ListItemText primary='Registros' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                 <QueryStats />
-              </ListItemIcon>
-              <ListItemText primary='Dashboards' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                 <NoteAlt />
-              </ListItemIcon>
-              <ListItemText primary='Anotações' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                 <Help />
-              </ListItemIcon>
-              <ListItemText primary='Ajuda' />
-            </ListItemButton>
-          </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeOutlined />
+            </ListItemIcon>
+            <ListItemText primary='Inicio' />
+          </ListItemButton>
+        </ListItem>
+        <div>
+          <Accordion expanded={true}>
+            <AccordionSummary>
+              <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                <Box sx={{ display: "flex", gap: 4 }}>
+                  <GroupAdd sx={{ opacity: 0.6 }} />
+                  <Typography component="span">Cadastros</Typography>
+                </Box>
+                <Box>
+                  <ChevronRight />
+                </Box>
+              </Box>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Alunos
+              </Typography>
+              <Typography>
+                Visitantes
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AppRegistration />
+            </ListItemIcon>
+            <ListItemText primary='Registros' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <QueryStats />
+            </ListItemIcon>
+            <ListItemText primary='Dashboards' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <NoteAlt />
+            </ListItemIcon>
+            <ListItemText primary='Anotações' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Help />
+            </ListItemIcon>
+            <ListItemText primary='Ajuda' />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
@@ -77,5 +96,5 @@ function MenuComponent() {
     </div>
   );
 }
-   
+
 export default MenuComponent;
