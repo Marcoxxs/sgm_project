d935e3b32eba58015352aa974aa56994aa2732af
Marcoxxs <mrkgab.069@gmail.com>
2026-06-25 00:02:47 -0300

    adição da barra de filtros

--- src/app/features/users/pages/RegistrosPage/index.js
+++ src/app/features/users/pages/RegistrosPage/index.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./style.css"
-import { Container } from "@mui/material";
+import { Box, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import FiltroComponent from "../../components/FiltroComponent";
 
 const laboratorios = ["Delta Lab", "Carnauba Lab"]
@@ -9,17 +9,50 @@ const onFiltrar = () => {
     console.log("filtrando")
 }
 
+const rows = [
+    {nome: "João da Silva", laboratorio: "Delta Lab", computador: "PC-01", data: "01/01/2024", tempoUtilizado: "2h"},
+    {nome: "Maria Souza", laboratorio: "Carnauba Lab", computador: "PC-02", data: "02/01/2024", tempoUtilizado: "1h"},
+    {nome: "Pedro Oliveira", laboratorio: "Delta Lab", computador: "PC-03", data: "03/01/2024", tempoUtilizado: "3h"},
+    {nome: "Ana Costa", laboratorio: "Carnauba Lab", computador: "PC-04", data: "04/01/2024", tempoUtilizado: "2h"},
+    {nome: "Lucas Lima", laboratorio: "Delta Lab", computador: "PC-05", data: "05/01/2024", tempoUtilizado: "1h"},
+    {nome: "Juliana Rocha", laboratorio: "Carnauba Lab", computador: "PC-06", data: "06/01/2024", tempoUtilizado: "2h"}
+];
+
 function RegistrosPage() {
     return (
         <Container>
-            <FiltroComponent onFiltrar={onFiltrar} semestres={semestres} laboratorios={laboratorios} showButton={true}/>
-                <div className="main">
-                    <div className="menu">
-                        <h1> Pagina Registros
-                        </h1>
-                        <Link to="/laboratorio">ir para o laboratorio</Link>
-                    </div>
-                </div>
+            <FiltroComponent onFiltrar={onFiltrar} semestres={semestres} laboratorios={laboratorios} showButton={true} />
+            <Box>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead >
+                            <TableRow>
+                                <TableCell align="right">Nome Completo</TableCell>
+                                <TableCell align="right">Laboratorio</TableCell>
+                                <TableCell align="right">Computador</TableCell>
+                                <TableCell align="right">Data</TableCell>
+                                <TableCell align="right">Tempo Utilizado</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.nome}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.nome}
+                                    </TableCell>
+                                    <TableCell align="right">{row.laboratorio}</TableCell>
+                                    <TableCell align="right">{row.computador}</TableCell>
+                                    <TableCell align="right">{row.data}</TableCell>
+                                    <TableCell align="right">{row.tempoUtilizado}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </Container>
     )
 }
